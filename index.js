c913825b3c8abee6e57243698600184cc5a5f407
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-09 02:50:57 -0300

    criei vetor e adicionei parametros para funções

--- index.js
+++ index.js
@@ -2,8 +2,20 @@
 var num = document.getElementById('txtn')
 var lista = document.getElementById('flista')
 var res = document.getElementById('res')
+var valores = []
 //Função adiconar numeros
 function Adicionar(){
     //validação
-    if(isNumero() && !inLista())
+    if(isNumero(num.value) && !inLista(num.value)){
+        window.alert('Continue!')
+    }
+    else{
+        window.alert('Dados inválidos!')
+    }
+    function isNumero(){
+
+    }
+    function inLista(){
+
+    }
 }
