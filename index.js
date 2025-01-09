ad54f120216ec80268b36ed26f8e5897feceaec0
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-09 02:46:26 -0300

    criei evento e função adicionar chamada pelo onclick

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 var num = document.getElementById('txtn')
 var lista = document.getElementById('flista')
 var res = document.getElementById('res')
+//Função adiconar numeros
+function Adicionar(){
+
+}
