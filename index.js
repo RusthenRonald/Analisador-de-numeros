349cfa004a24950b698d98d521bd2336b4103eea
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-09 02:53:45 -0300

    verifico se n esta entre 1 e 100 e retorno true ou false

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ var valores = []
 //Função adiconar numeros
 function Adicionar(){
     //validação
-    if(isNumero(num.value) && !inLista(num.value)){
+    if(isNumero(num.value) && !inLista(num.value,valores)){
         window.alert('Continue!')
     }
     else{
         window.alert('Dados inválidos!')
     }
-    function isNumero(){
-
+    function isNumero(n){
+        if(Number(n)>=1 && Number(n)<=100){
+            return true
+        }
+        else{
+            return false 
+        }
     }
     function inLista(){
 
